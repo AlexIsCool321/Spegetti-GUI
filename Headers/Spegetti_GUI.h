e9e9dd46e8e2a49a05c280407d9b547aeea8e517
AlexIsCool321 <alex.m.diehl1@gmail.com>
2024-12-06 22:29:02 -0600

    Project Set Up
    -set up the project

---
+++ Headers/Spegetti_GUI.h
@@ -0,0 +1,14 @@
+#ifndef Spegetti_GUI_H
+#define Spegetti_GUI_H
+
+#includes <iostream>
+
+namespace Spegetti_GUI
+{
+	
+	void Init();
+	
+}
+
+
+#endif Spegetti_GUI_H
---
+++ Sources/Spegetti_GUI.cpp
@@ -0,0 +1,13 @@
+#include <Spegetti_GUI.h>
+
+namespace Spegetti_GUI
+{
+	
+	void Init()
+	{
+		
+		std::cout << "Hello, World!" << std::endl;
+		
+	}
+	
+}
